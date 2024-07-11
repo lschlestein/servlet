61e4b2f61fd9e1e12abefd4cee72b9c1f3b5a824
Lucas Schlestein <lschlestein@gmail.com>
2024-07-11 19:58:45 -0300

    UPDATE Login Page

--- src/main/java/com/servlet/aula/aulaservlet0807/HelloServlet.java
+++ src/main/java/com/servlet/aula/aulaservlet0807/HelloServlet.java
@@ -1,5 +1,6 @@
 package com.servlet.aula.aulaservlet0807;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,15 +14,17 @@ public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String action = request.getServletPath();
         if (action.equals("/login")) {
             String email = request.getParameter("email");
             String password = request.getParameter("password");
             System.out.println("Email: " + email + " Password: " + password);
+            request.setAttribute("email", email);
             if (email.equals("lucas@mail.com") && password.equals("123")) {
-
-
+                request.getRequestDispatcher("welcome.jsp").forward(request,response);
+            }else{
+                request.getRequestDispatcher("loginFail.jsp").forward(request,response);
             }
         }
     }
--- src/main/webapp/loginFail.jsp
+++ src/main/webapp/loginFail.jsp
@@ -1,17 +1,17 @@
 <%--
   Created by IntelliJ IDEA.
   User: Lucas
-  Date: 08/07/2024
-  Time: 21:36
+  Date: 11/07/2024
+  Time: 19:38
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Welcome!</title>
+    <title>Login fail!</title>
 </head>
 <body>
-<h1>Welcome User! Your email is: ${email} </h1>
+<h1>Can't find user with email: ${email}</h1>
 <a href="index.jsp">Home</a>
 </body>
 </html>
